9c44e570a24d60b7d4d1e953efa6ff202d337923
Sulabh Bansal <bansalsulabh13@gmail.com>
2021-02-08 20:13:51 +0530

    adding blur javascript thing in navbar

---
+++ site/js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+	$("#navbarToggle").blur(function (event) {
+		var screenWidth = window.innerWidth;
+		if (screenWidth<768)
+		{
+			$("#collapsable-nav").collapse('hide');
+		}
+	});
+});
